49cced2af2df2636fe27dc8dba113f0c2fb2908c
Mun Kit <leoan96@hotmail.com>
2021-06-18 23:24:46 +0800

    commit till lecture 46: understanding portls

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import Users from "./user/pages/users.pages";
 import NewPlace from "./places/pages/new-place.pages";
+import MainNavigation from "./shared/components/navigation/main-navigation.component";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Users />
-        </Route>
-        <Route exact path="/places/new">
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 };
