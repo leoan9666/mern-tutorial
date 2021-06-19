6ca682f8f8a7611abbda61475a1b12630901358c
leoan9666 <85979876+leoan9666@users.noreply.github.com>
2021-06-19 12:12:46 +0800

    Develop (#2)
    * added user-item, user-list, user.pages, and user-list, user-item css files
    
    * add card and avatar components and their respective css, modify user-item.component.css card class by commenting padding
    
    * commit till lecture 46: understanding portls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+import Users from "./user/pages/users.pages";
+import NewPlace from "./places/pages/new-place.pages";
+import MainNavigation from "./shared/components/navigation/main-navigation.component";
 
 const App = () => {
-  return <h1>Let's start!</h1>;
+  return (
+    <BrowserRouter>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/places/new">
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
