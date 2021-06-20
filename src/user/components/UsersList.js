d6e1b047d32c9ba4acef670c6977888d0d6e0a15
Mun Kit <leoan96@hotmail.com>
2021-06-20 09:45:53 +0800

    update code with course code

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import "./users-list.component.css";
+import UserItem from './UserItem';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
 
-import UserItem from "./user-item.component";
-import Card from "../../shared/components/ui-elements/card.component";
-
-const UsersList = (props) => {
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
@@ -18,7 +17,7 @@ const UsersList = (props) => {
 
   return (
     <ul className="users-list">
-      {props.items.map((user) => (
+      {props.items.map(user => (
         <UserItem
           key={user.id}
           id={user.id}
